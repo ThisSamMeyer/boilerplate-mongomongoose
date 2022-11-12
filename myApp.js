ac9b58aa5dc53f204cc0767ebfe584242c80a479
Samantha Meyer <slmeyer90@gmail.com>
2022-11-11 21:24:15 -0500

    bugs

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 // #1 - Install and Set Up Mongoose
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
